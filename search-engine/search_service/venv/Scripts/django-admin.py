eb9b5ad933b758291a7835d5e019d4de7a243e99
Hitesh Shetty <hitesh.shetty.k90@gmail.com>
2019-04-24 00:29:59 -0400

    Base Search-Engine module for yelp-2.0
    Committing the base search-engine module for this project.

---
+++ search-engine/search_service/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!a:\stevens\github-repos\yelp-2.0\search-engine\search_service\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
